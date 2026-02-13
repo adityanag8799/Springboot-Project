ba78af079a4e7a83ae1e52b7c94917caccbed190
adityanag8799 <adityanag8799@gmail.com>
2026-02-13 21:28:44 +0530

    Inventory test file

--- inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
+++ inventory-service/src/test/java/com/techie/microservices/inventory/InventoryServiceApplicationTests.java
@@ -1,56 +1,56 @@
-// package com.techie.microservices.inventory;
+package com.techie.microservices.inventory;
 
-// import io.restassured.RestAssured;
-// import org.junit.jupiter.api.BeforeEach;
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
-// import org.springframework.boot.test.web.server.LocalServerPort;
-// import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
-// import org.springframework.context.annotation.Import;
-// import org.testcontainers.junit.jupiter.Container;
-// import org.testcontainers.junit.jupiter.Testcontainers;
-// import org.testcontainers.mysql.MySQLContainer;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Import;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
-// import static org.junit.jupiter.api.Assertions.assertFalse;
-// import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-// @Testcontainers
-// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-// class InventoryServiceApplicationTests {
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class InventoryServiceApplicationTests {
 
-// 	@Container
-// 	@ServiceConnection
-// 	static MySQLContainer mySQLContainer =
-// 			new MySQLContainer("mysql:8.3.0");
+	@Container
+	@ServiceConnection
+	static MySQLContainer mySQLContainer =
+			new MySQLContainer("mysql:8.3.0");
 
-// 	@LocalServerPort
-// 	private Integer port;
+	@LocalServerPort
+	private Integer port;
 
-// 	@BeforeEach
-// 	void setup() {
-// 		RestAssured.baseURI = "http://localhost";
-// 		RestAssured.port = port;
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURI = "http://localhost";
+		RestAssured.port = port;
 
-// //		RestAssured.baseURI = "http://localhost:" + port;
-// 	}
+//		RestAssured.baseURI = "http://localhost:" + port;
+	}
 
-// 	@Test
-// 	void shouldReadInventory() {
-// 		System.out.println("PORT = " + port);
-// 		var response = RestAssured.given()
-// 				.when()
-// 				.get("/api/inventory?skuCode=iphone_14&quantity=11")
-// 				.then()
-// 				.statusCode(200)
-// 				.extract().as(Boolean.class);
-// 		assertTrue(response);
+	// @Test
+	// void shouldReadInventory() {
+	// 	System.out.println("PORT = " + port);
+	// 	var response = RestAssured.given()
+	// 			.when()
+	// 			.get("/api/inventory?skuCode=iphone_14&quantity=11")
+	// 			.then()
+	// 			.statusCode(200)
+	// 			.extract().as(Boolean.class);
+	// 	assertTrue(response);
 
-// 		var negativeResponse = RestAssured.given()
-// 				.when()
-// 				.get("/api/inventory?skuCode=iphone_14&quantity=110")
-// 				.then()
-// 				.statusCode(200)
-// 				.extract().as(Boolean.class);
-// 		assertFalse(negativeResponse);
-// 	}
-// }
+	// 	var negativeResponse = RestAssured.given()
+	// 			.when()
+	// 			.get("/api/inventory?skuCode=iphone_14&quantity=110")
+	// 			.then()
+	// 			.statusCode(200)
+	// 			.extract().as(Boolean.class);
+	// 	assertFalse(negativeResponse);
+	// }
+}
